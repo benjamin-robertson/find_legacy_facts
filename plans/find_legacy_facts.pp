6a0ea5926acf46741e002d722e7cb58b8fc947cf
ben <benrobertson9876@gmail.com>
2024-10-29 17:43:18 +1100

    Update plan

--- plans/find_legacy_facts.pp
+++ plans/find_legacy_facts.pp
@@ -24,8 +24,8 @@ plan find_legacy_facts::find_legacy_facts (
   }
 
   # Update facts
-  without_default_logging() || { run_plan('facts', targets => $pe_target) }
   $pe_target_certname = $pe_target.map | Hash $node | { $node['certname'] }
+  without_default_logging() || { run_plan(facts, targets => $pe_target_certname) }
   out::message("pe_target_certname is ${pe_target_certname}")
 
   $task_results = run_task('find_legacy_facts::init', $pe_target_certname, { 'environment' => $environment, 'check_ruby' => $check_ruby, 'environment_path' => $pe_target.facts['puppet_environmentpath'], '_catch_errors' => true })
